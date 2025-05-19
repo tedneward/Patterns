e5fee2a29d38f69d81e979470f017ae08db20874
Ted Neward <ted@tedneward.com>
2025-05-18 18:03:50 -0700

    Added Swift CoR

--- Behavioral/ChainOfResponsibility/swift/README.md
+++ Behavioral/ChainOfResponsibility/swift/README.md
@@ -1,4 +1,6 @@
 # Chain of Responsibility: Swift
 Uses command-line swift, and as such, should be available/accessible for execution from any OS that supports Swift at the command line (eg., specifically, Linux, though WSL may be a possibility too).
 
+Like other languages, this implementation has multiple versions. In the first version, we manage the chain explicitly as part of the protocol. In the second version, however, we use an Array to hold the chain, which is distinctly different from other languages--this is because Swift has no built-in linked list collection class as part of Foundation.
+
 To run, use `swift run` in this directory.
--- Behavioral/ChainOfResponsibility/swift/Sources/main.swift
+++ Behavioral/ChainOfResponsibility/swift/Sources/main.swift
@@ -2,74 +2,133 @@ struct Currency {
     let amount: Double
 }
 
-protocol ATMHandler {
-    
-    var nextHandler: ATMHandler? { get set }
-    
-    func handle(request: Currency)
-}
-
-class ATM {
-    private var chain: ATMHandler?
-
-    init() {
-        let fiftyDollarHandler = FiftyDollarHandler()
-        let twentyDollarHandler = TwentyDollarHandler()
-        let tenDollarHandler = TenDollarHandler()
+struct v1 {
+    protocol ATMHandler {
         
-        fiftyDollarHandler.nextHandler = twentyDollarHandler
-        twentyDollarHandler.nextHandler = tenDollarHandler
+        var nextHandler: ATMHandler? { get set }
         
-        chain = fiftyDollarHandler
+        func handle(request: Currency)
     }
-    
-    func dispense(request: Currency) {
-        chain?.handle(request: request)
+
+    class ATM {
+        private var chain: ATMHandler?
+
+        init() {
+            let fiftyDollarHandler = FiftyDollarHandler()
+            let twentyDollarHandler = TwentyDollarHandler()
+            let tenDollarHandler = TenDollarHandler()
+            
+            fiftyDollarHandler.nextHandler = twentyDollarHandler
+            twentyDollarHandler.nextHandler = tenDollarHandler
+            
+            chain = fiftyDollarHandler
+        }
+        
+        func dispense(request: Currency) {
+            chain?.handle(request: request)
+        }
+    }
+
+    class FiftyDollarHandler: ATMHandler {
+        var nextHandler: ATMHandler?
+        
+        func handle(request: Currency) {
+            let count = Int(request.amount / 50)
+            if count > 0 {
+                print("Dispensing \(count) $50 bill(s)")
+            }
+            let remainingAmount = request.amount - Double(count * 50)
+            if remainingAmount > 0 {
+                nextHandler?.handle(request: Currency(amount: remainingAmount))
+            }
+        }
+    }
+    class TwentyDollarHandler: ATMHandler {
+        var nextHandler: ATMHandler?
+        
+        func handle(request: Currency) {
+            let count = Int(request.amount / 20)
+            if count > 0 {
+                print("Dispensing \(count) $20 bill(s)")
+            }
+            let remainingAmount = request.amount - Double(count * 20)
+            if remainingAmount > 0 {
+                nextHandler?.handle(request: Currency(amount: remainingAmount))
+            }
+        }
+    }
+    class TenDollarHandler: ATMHandler {
+        var nextHandler: ATMHandler?
+        
+        func handle(request: Currency) {
+            let count = Int(request.amount / 10)
+            if count > 0 {
+                print("Dispensing \(count) $10 bill(s)")
+            }
+            let remainingAmount = request.amount - Double(count * 10)
+            if remainingAmount > 0 {
+                nextHandler?.handle(request: Currency(amount: remainingAmount))
+            }
+        }
+    }
+
+}
+
+struct v2 {
+    protocol ATMHandler {        
+        func handle(request: Currency) -> Currency
+    }
+
+    class ATM {
+        private var chain: [ATMHandler] = []
+
+        init() {
+            let fiftyDollarHandler = FiftyDollarHandler()
+            let twentyDollarHandler = TwentyDollarHandler()
+            let tenDollarHandler = TenDollarHandler()
+            
+            chain += [fiftyDollarHandler, twentyDollarHandler, tenDollarHandler]
+        }
+        
+        func dispense(request: Currency) {
+            var amount = request
+            for handler in chain {
+                amount = handler.handle(request: amount)
+            }        
+        }
+    }
+
+    class FiftyDollarHandler: ATMHandler {
+        func handle(request: Currency) -> Currency{
+            let count = Int(request.amount / 50)
+            let remainder = request.amount - Double(count * 50)
+            if count > 0 {
+                print("Dispensing \(count) $50 bill(s)")
+            }
+            return Currency(amount: remainder)
+        }
+    }
+    class TwentyDollarHandler: ATMHandler {
+        func handle(request: Currency) -> Currency{
+            let count = Int(request.amount / 20)
+            let remainder = request.amount - Double(count * 50)
+            if count > 0 {
+                print("Dispensing \(count) $20 bill(s)")
+            }
+            return Currency(amount: remainder)
+        }
+    }
+    class TenDollarHandler: ATMHandler {
+        func handle(request: Currency) -> Currency {
+            let count = Int(request.amount / 10)
+            let remainder = request.amount - Double(count * 50)
+            if count > 0 {
+                print("Dispensing \(count) $10 bill(s)")
+            }
+            return Currency(amount: remainder)
+        }
     }
 }
 
-class FiftyDollarHandler: ATMHandler {
-    var nextHandler: ATMHandler?
-    
-    func handle(request: Currency) {
-        let count = Int(request.amount / 50)
-        if count > 0 {
-            print("Dispensing \(count) $50 bill(s)")
-        }
-        let remainingAmount = request.amount - Double(count * 50)
-        if remainingAmount > 0 {
-            nextHandler?.handle(request: Currency(amount: remainingAmount))
-        }
-    }
-}
-class TwentyDollarHandler: ATMHandler {
-    var nextHandler: ATMHandler?
-    
-    func handle(request: Currency) {
-        let count = Int(request.amount / 20)
-        if count > 0 {
-            print("Dispensing \(count) $20 bill(s)")
-        }
-        let remainingAmount = request.amount - Double(count * 20)
-        if remainingAmount > 0 {
-            nextHandler?.handle(request: Currency(amount: remainingAmount))
-        }
-    }
-}
-class TenDollarHandler: ATMHandler {
-    var nextHandler: ATMHandler?
-    
-    func handle(request: Currency) {
-        let count = Int(request.amount / 10)
-        if count > 0 {
-            print("Dispensing \(count) $10 bill(s)")
-        }
-        let remainingAmount = request.amount - Double(count * 10)
-        if remainingAmount > 0 {
-            nextHandler?.handle(request: Currency(amount: remainingAmount))
-        }
-    }
-}
-
-let atm = ATM()
+let atm = v2.ATM()
 atm.dispense(request: Currency(amount: 190))
