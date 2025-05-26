0448b683a4650d1e630cbd167de5ae0e5c24d10c
Ted Neward <ted@tedneward.com>
2025-05-26 18:35:55 -0500

    F# v3 version

--- Behavioral/ChainOfResponsibility/fsharp/Program.fs
+++ Behavioral/ChainOfResponsibility/fsharp/Program.fs
@@ -59,31 +59,31 @@ module v3 =
                 if amount.Amount >= 50 then
                     let count = amount.Amount / 50
                     printfn "Dispensing %d fifty dollar bills" count
-                    [Currency(amount.Amount - (count * 50))]
+                    Currency(amount.Amount - (count * 50))
                 else
-                    [amount]
+                    amount
 
             fun amount -> 
                 if amount.Amount >= 20 then
                     let count = amount.Amount / 20
                     printfn "Dispensing %d twenty dollar bills" count
-                    [Currency(amount.Amount - (count * 20))]
+                    Currency(amount.Amount - (count * 20))
                 else
-                    [amount]
+                    amount
 
             fun amount -> 
                 if amount.Amount >= 10 then
                     let count = amount.Amount / 10
                     printfn "Dispensing %d ten dollar bills" count
-                    [Currency(amount.Amount - (count * 10))]
+                    Currency(amount.Amount - (count * 10))
                 else
-                    [amount]
+                    amount
         ]
 
         member this.Dispense(amount : Currency) =
             let mutable remaining = amount
             for handler in chain do
-                remaining <- handler.Dispense(remaining)
+                remaining <- handler(remaining)
 
             if remaining.Amount > 0 then
                 printfn "Cannot dispense remaining amount: %d" remaining.Amount
--- Behavioral/ChainOfResponsibility/fsharp/README.md
+++ Behavioral/ChainOfResponsibility/fsharp/README.md
@@ -3,11 +3,7 @@ There are multiple implementations of the ATM in this directory.
 
 * **v2** uses a custom interface for the dispensation call and the F#-standard `list` type to maintain the chain as a whole. However, since now each element doesn't decide whether to continue processing the request or not, we must "signal" the interim state of the request by returning some kind of indication whether more work is required or not. In this particular case, we key off of the `Currency` amount requested--as each stage in the chain dispenses cash, if it did not dispense the requested amount entirely, it returns the remaining amount that requires dispensation.
 
-
-
-
-
-* **ATMv3** uses the standard `Func<>` type along with `List` to minimize the need for custom types entirely, but when we do this, we lose any ability to have more than one method on the interface, as well as to define top-level types that describe each different processing type more explicitly.
+* **v3** goes to function values directly, rather than making use of an interim type. This is more functional in nature, but doing so loses any ability to have more than one method on the interface, as well as to define top-level types that describe each different processing type more explicilty.
 
 Note that in a more production-worthy implementation, the ATM types would handle the input more effectively, so that the `Program.Main` code would be simpler and not have to handle the "divisible by 10" requirement explicitly.
 
